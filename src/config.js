d3da2d87d5c267a0b750b9bd97df5a369f27359a
eXoterr <stellarium@exoterr.anonaddy.me>
2022-10-02 15:46:23 +0300

    moved apikey to env vars

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    APIKEY : "00000000-0000-0000-0000-000000000000", // Set your key from https://kinopoiskapiunofficial.tech
+    APIKEY : (process.env.APIKEY), // Set your key from https://kinopoiskapiunofficial.tech
     ENABLE_PLAYER : false, // Enables online in-page player
     PLAYER_URL : "https://yohoho.cc/yo.js" // Url (mirror) for load player from
 } 
